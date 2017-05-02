038b9d8265d12f21b1a24def1c604876952b4331
marctaylor <mtaylor@fwparker.org>
2017-05-02 11:09:12 -0500

    commit

--- marc_1.py
+++ marc_1.py
@@ -1,1 +1,13 @@
 #Marc's file
+# Beautiful Soup
+
+import urllib.request
+from bs4 import BeautifulSoup
+
+url = "http://www.fandango.com/regalwebsterplace11_aaaxr/theaterpage?date=5%2f2%2f2017"
+
+page = urllib.request.urlopen(url)
+soup = BeautifulSoup(page.read(), "html.parser")
+
+def pull_dates():
+    return []
