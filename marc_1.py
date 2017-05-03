d7cbdcbc00c68de776c0bce358aefe184dbfcfcf
marctaylor <mtaylor@fwparker.org>
2017-05-03 08:58:46 -0500

    commit

--- marc_1.py
+++ marc_1.py
@@ -4,7 +4,7 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
-
+# Dates
 def get_date():
 
     url = "http://www.fandango.com/regalwebsterplace11_aaaxr/theaterpage?date=5%2f2%2f2017"
@@ -14,26 +14,34 @@ def get_date():
 
 
     my_week_days = [x.text for x in soup.findAll("div", {"class": "date-week-day"})][:7]
-    print(my_week_days)
 
     my_months = [x.text for x in soup.findAll("div", {"class": "date-month"})][:7]   # target a specific table
-    print(my_months)
 
     my_day = [x.text for x in soup.findAll("div", {"class": "date-day"})][:7]
-    print(my_day)
 
     date_list = []
 
     for i in range(len(my_week_days)):
         date_list.append(my_week_days[i]+", "+ my_months[i] + " " + my_day[i])
     print(date_list)
-    return date_list
+
+    url_list = [x['href'] for x in soup.findAll("a", {"class": "date-area"})]
+    print(url_list)
+    return date_list, url_list
+
+
+
+
+
+
+
+# Movies
 
 
 
 
 if __name__ == "__main__":
-    pass
+    get_date()
 
 
 
@@ -42,13 +50,10 @@ if __name__ == "__main__":
 #rows = my_table.findAll("tr")
 
 
-# Dates
-#dates = []
-#for row in rows:
 
 
 
-# Movies
+
 
 
 
