4b0d2a83d3f5ab7848fa4d037122b11d2d2f226a
Charlie <cmoog@fwparker.org>
2017-05-09 18:37:25 -0500

    /

--- interface.py
+++ interface.py
@@ -151,7 +151,7 @@ class App():
         self.purchase_button = Button(master, text="Purchase", command=lambda:purchase(self, self.response_label, self.date.get(), self.movie.get(), self.time.get(), str(self.ticket_quant.get())))
         self.purchase_button.grid(row=6, column=1, columnspan=2)
 
-        def purchase(self, label, date, movie, time, quantity, movie_list, time_list):
+        def purchase(self, label, date, movie, time, quantity):
             '''RUN ELIZA'S FUNCTION'''
             label_text.set("Thank you for your purchase!\n Receipt:\nDate: " + str(date) + "\nMovie: " + str(movie) + "\nTime: " + str(time) + "\nQuantity: " + str(quantity))
             label.grid(row=7, column=1, columnspan=2)
